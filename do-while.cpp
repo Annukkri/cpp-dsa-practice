b893811d76c4da7d448582b655c358695370f532
Annu Kumari <annustmk@gmail.com>
2026-07-29 08:42:04 +0000

    do - while 2

--- do-while.cpp
+++ do-while.cpp
@@ -1,11 +1,38 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i = 1;
-    do{
-        cout << i << endl;
-        i++;
-    }while (i<=10);
-    cout << endl;
-    return 0;
+//     int i = 1;
+//     do{
+//         cout << i << endl;
+//         i++;
+//     }while (i<=10);
+//     cout << endl;
+//     return 0;
+// }
+
+// int i = 4;
+// cout << (i++) << endl;
+// return 0;
+// }
+
+// for (int i = 1; i<=3; i++)
+// {
+//     for (int j = 1; j <= 3; j++)
+//     {
+//         cout << i << j << endl;
+//     }
+// }
+// return 0;
+// }
+
+
+for (int i = 1; i<=3; i++)
+{
+    for (int j = 1; j <= 3; j++)
+    {
+        cout << i * j << endl;
+    }
 }
+return 0;
+}
+
